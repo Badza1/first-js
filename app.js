7e58f8259b78e8e54ec68486df0f979f9cd02cdf
Badza1 <jagernout77@gmail.com>
2022-06-25 18:24:34 +0400

    first-js

---
+++ app.js
@@ -0,0 +1,29 @@
+var myVariables = 'gio';
+
+
+let und , x, y;
+und=1;
+x='first js';
+y="First Js";
+
+document.getElementById('js').innerHTML = x;
+document.getElementById('js2').innerText = y;
+
+
+
+let numb = 66;
+let array = {};
+let str = 'firstjs';
+let bool = true;
+let boolf = false;
+let obj ={};
+let functions  = function(){} ;
+
+
+
+
+
+console.log(9 == 9);
+
+console.log(typeof obj);
+
