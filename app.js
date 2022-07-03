294c4415d73806b98d2975fe7f7b8c06f405ae3e
Badza1 <jagernout77@gmail.com>
2022-07-03 18:34:44 +0400

    first

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
 var myVariables = 'gio';
 
 
-let und , x, y;
-und=1;
-x='first js';
-y="First Js";
+let stri = "Aa12#!&$"
+if (stri.length < 8 ) {
+    console.error('უნდა შეიცავდეს მინიმუმ 8 სიმბოლოს')
+}
 
-document.getElementById('js').innerHTML = x;
-document.getElementById('js2').innerText = y;
+let mid = `ეს არის ჩემი პირველი java script კოდი`;
+let finalmid = mid.slice(13,20);
+console.log(finalmid);
+
+let rep =`(ეს არის ჩემი პირველი java script კოდი)`;
+let finalrep = rep.replaceAll('java script' , 'js')
+console.log(finalrep);
+
+let split =`(ეს არის ჩემი პირველი java script კოდი)`;
+let finalsplit = split.split(' ')
+console.log(finalsplit);
+
+let start =`ეს არის ჩემი პირველი java script კოდი)`;
+let finalstart = start.startsWith('ეს')
+console.log(finalstart);
+
+let search =`(ეს არის ჩემი პირველი java script კოდი)`;
+let finalsearch = search.search('java')
+console.log(finalsearch);
+
+let up =`this is my first java script code`;
+let finalup =up.toUpperCase('this is my first java script code');
+console.log(finalup);
+
+let tr =`           ეს არის ჩემი პირველი java script კოდი          `;
+let finaltr =tr.trim('');
+console.log(finaltr);
 
 
 
-let numb = 66;
-let array = {};
-let str = 'firstjs';
-let bool = true;
-let boolf = false;
-let obj ={};
-let functions  = function(){} ;
-
-
-
-
-
-console.log(9 == 9);
-
-console.log(typeof obj);
 
---
+++ test/asdasd.js
@@ -0,0 +1,55 @@
+// const profilecard = document.querySelector(".card-container");
+
+// function createcard([Pro,img,user,title,loc,p,primary,ghost,,skillsh,skillsl,skillsu,skillsf,skillshh,skillsc,skillsj,skillsr,skillsn ]){
+//     let code = `
+//     <span class="pro">${pro}</span>
+//     <img src="${img}" alt="${user}"/>
+//     <h3 class="title" >${title}</h3>
+//     <h6 class="loc">${loc}</h6>
+//     <p class="p" >${p}</p>
+//     <div class="buttons">
+//         <button class="primary">
+//         ${primary}
+//         </button>
+//         <button class="primary ghost">
+//         ${ghost}
+//         </button>
+//     </div>
+//     <div class="skills">
+//         <h6 class="skillsh" >${skillsh}</h6>
+//         <ul>
+//             <li class="skillsu" >${skillsu}</li>
+//             <li class="skillslif" >${skillsf}</li>
+//             <li class="skillslih" >${skillshh}</li>
+//             <li class="skillslic" >${skillsc}</li>
+//             <li class="skillslij" >${skillsj}</li>
+//             <li class="skillslir" >${skillsr}</li>
+//             <li class="skillslin" >${skillsn}</li>
+//         </ul>
+//     </div>
+    
+//     `
+//     ;
+//     profilecard.innerHTML += code;
+// }
+
+// let item1 =["PRO",
+//     "https://media-exp1.licdn.com/dms/image/C5603AQF2Pf7Yy3vWmg/profile-displayphoto-shrink_200_200/0/1517237678354?e=1657756800&v=beta&t=ZDu_sGmgCRxEJzFAhTLbYmuCidr2br3O4zhTL4LhWjQ",
+//     "user",
+//     "Rahul Yaduvanshi",
+//     "lNODIA, U.P.",
+//     "User interface designer and front-end developer",
+//     "Message",
+//     "Following",
+//     "SKILLS",
+//     "UI/UX",
+//     "Front End Development",
+//     "HTML",
+//     "CSS",
+//     "JavaScript",
+//     "React",
+//     "Node"],
+    
+
+
+// createcard(item1);
